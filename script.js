aa3ef71950857519f44c1b8ac0a4be6e478c4e83
Samuel Stiksa <samsti@email.cz>
2023-01-09 20:36:28 +0100

    final version

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let username = document.forms['form']['username_input']
 let password = document.forms['form']['password_input']
 let usernameError = document.getElementById('username_error')
@@ -7,7 +6,7 @@ let submitButton = document.getElementById('submit')
 let form = document.querySelectorAll('form')[0]
 
 
-form.addEventListener('submit', (e) =>{
+form.addEventListener('submit', (e) => {
 
     e.preventDefault();
 
@@ -16,10 +15,25 @@ form.addEventListener('submit', (e) =>{
         usernameError.style.color = "red"
         username.style.border = "1px solid red"
 
+    }else if (username.value !== ''){
+        usernameError.style.color = "white"
+        username.style.border = "1px solid #ccc"
     }
-    if(password.value === "" || password.value.length < 6){
+    if (password.value === "" || password.value.length < 6) {
 
         passwordError.style.color = "red"
         password.style.border = "1px solid red"
+
+    }else if (password.value !== '' && password.value.length >= 6) {
+        passwordError.style.color = "white"
+        password.style.border = "1px solid #ccc"
     }
+
+    submitButton.addEventListener('click', () =>{
+        location.href='home.html';
+    })
+
+
 })
+
+
